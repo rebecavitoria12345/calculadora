12414133128296ca626da9dc47fc41a6d53529be
rebecavitoria12345 <rebeca.vitoria.pereira@escola.pr.gov.br>
2025-09-09 12:18:01 -0300

    askjda

--- script.js
+++ script.js
@@ -4,11 +4,27 @@ function imc(){
     const nome = document.getElementById('nome').value;
     const altura = document.getElementById('altura').value;
     const peso = document.getElementById('peso').value;
-    const resultado = document.getElementById('resultado');
+    const resultado = document.getElementById
+    ('resultado');
 
     const valorIMC = (peso/(altura*altura)).toFixed(1);
 
     resultado.textContent = `${nome} seu IMC é ${valorIMC}!`;
 }
 
-calcular.addEventlistenner('click',imc);
+ let classificacao ="";
+
+    if (valorIMC <18.5){
+        classificacao = "abaixo do peso": 
+    }else if(valorIMC < 25){
+        classificacao = "peso ideal";
+    }else if(valorIMC < 30){
+        classificacao = "levemente acima do peso";
+    }else {
+        classificacao = "obesidade"
+    }
+    resultado.textContent = `${nome} seu IMC é ${valoIMC} , sua classificacao é ${classificacao}`;
+
+}
+
+calcular.addEventListener('click',imc);
